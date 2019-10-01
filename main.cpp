fe67b5e924792433ac9f1388dae1003e55f210cd
TheRealBazhen <andre.bazhenov30012001@gmail.com>
2019-10-02 01:42:47 +0300

    Add a few tests

--- main.cpp
+++ main.cpp
@@ -27,21 +27,7 @@ void PrintStateMachine(const fsm::StateMachine& machine) {
     }
 }
 
-void TestSample() {
-    // sample from seminar
-    fsm::StateMachine source(6);
-    uint8_t a = 'a', b = 'b', e = fsm::StateMachine::empty_transfer_symbol;
-    source.AddTransfer(0, 1, a);
-    source.AddTransfer(1, 2, a);
-    source.AddTransfer(1, 3, b);
-    source.AddTransfer(1, 4, e);
-    source.AddTransfer(2, 1, b);
-    source.AddTransfer(3, 1, a);
-    source.AddTransfer(4, 4, a);
-    source.AddTransfer(4, 5, b);
-    source.AddTransfer(4, 0, e);
-    source.AddTransfer(5, 4, a);
-    source.AddFinalState(4);
+void OptimizeMachine(const fsm::StateMachine& source) {
     auto single_transfers = fsm::RemoveEmptyTransfers(source);
     auto determined = fsm::BuildDeterminedMachine(single_transfers);
     auto minimized = fsm::Minimize(determined);
@@ -59,7 +45,63 @@ void TestSample() {
     PrintStateMachine(minimized);
 }
 
+void TestSample() {
+    // sample from seminar
+    fsm::StateMachine source(6);
+    uint8_t a = 'a', b = 'b', e = fsm::StateMachine::empty_transfer_symbol;
+    source.AddTransfer(0, 1, a);
+    source.AddTransfer(1, 2, a);
+    source.AddTransfer(1, 3, b);
+    source.AddTransfer(1, 4, e);
+    source.AddTransfer(2, 1, b);
+    source.AddTransfer(3, 1, a);
+    source.AddTransfer(4, 4, a);
+    source.AddTransfer(4, 5, b);
+    source.AddTransfer(4, 0, e);
+    source.AddTransfer(5, 4, a);
+    source.AddFinalState(4);
+    OptimizeMachine(source);
+}
+
+void HomeWork1() {
+    fsm::StateMachine source(8);
+    uint8_t a = 'a', b = 'b', e = fsm::StateMachine::empty_transfer_symbol;
+    source.AddTransfer(0, 0, a);
+    source.AddTransfer(0, 0, b);
+    source.AddTransfer(0, 1, a);
+    source.AddTransfer(0, 7, a);
+    source.AddTransfer(1, 2, a);
+    source.AddTransfer(1, 6, b);
+    source.AddTransfer(2, 2, a);
+    source.AddTransfer(2, 2, b);
+    source.AddTransfer(2, 3, b);
+    source.AddTransfer(3, 4, b);
+    source.AddTransfer(4, 4, b);
+    source.AddTransfer(4, 5, a);
+    source.AddTransfer(5, 5, b);
+    source.AddTransfer(6, 4, a);
+    source.AddTransfer(7, 6, b);
+    source.AddFinalState(5);
+    OptimizeMachine(source);
+}
+
+void HomeWork2() {
+    fsm::StateMachine source(5);
+    uint8_t a = 'a', b = 'b', e = fsm::StateMachine::empty_transfer_symbol;
+    source.AddTransfer(0, 1, a);
+    source.AddTransfer(1, 0, e);
+    source.AddTransfer(1, 2, a);
+    source.AddTransfer(1, 3, b);
+    source.AddTransfer(2, 1, b);
+    source.AddTransfer(3, 1, a);
+    source.AddTransfer(3, 4, b);
+    source.AddTransfer(4, 3, a);
+    source.AddFinalState(1);
+    OptimizeMachine(source);
+}
+
+
 int main() {
-    TestSample();
+    HomeWork2();
     return 0;
 }
